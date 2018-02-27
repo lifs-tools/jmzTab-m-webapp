381ae58b2f5a11113659d796d2a490b6a1b2655d
Nils Hoffmann <nils.hoffmann@isas.de>
2018-02-27 14:21:17 +0100

    Updated suppressions for javax.ws.rs.

--- src/main/java/de/isas/lipidomics/mztab/validator/webapp/controller/ValidationController.java
+++ src/main/java/de/isas/lipidomics/mztab/validator/webapp/controller/ValidationController.java
@@ -37,8 +37,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.multipart.MultipartException;
 import org.springframework.web.servlet.ModelAndView;
